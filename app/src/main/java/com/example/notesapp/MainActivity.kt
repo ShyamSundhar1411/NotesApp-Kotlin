2685f0517a32f96d76d60f427d8d79ad230f00da
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-06-03 23:17:54 +0530

    feat: added notes screen scaffold

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.notesapp.screen.NoteScreen
 import com.example.notesapp.ui.theme.NotesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,13 +20,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NotesAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                }
+            MyApp {
+                NoteScreen()
             }
         }
     }
 }
 
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+    NotesAppTheme {
+        content()
+    }
+}
 
+@Preview
+@Composable
+fun MyAppPreview(){
+    MyApp{
+        NoteScreen()
+    }
+}
--- app/src/main/java/com/example/notesapp/screen/NoteScreen.kt
+++ app/src/main/java/com/example/notesapp/screen/NoteScreen.kt
@@ -1,2 +1,40 @@
 package com.example.notesapp.screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.notesapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun NoteScreen(){
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(text = stringResource(id = R.string.app_name))
+            },
+                actions = {
+                    Icon(imageVector = Icons.Rounded.Notifications, contentDescription = "Icon" )
+                }
+            )
+
+        }
+    ) {
+        innerPadding -> Box(modifier = Modifier.padding(innerPadding)){
+
+        }
+    }
+}
